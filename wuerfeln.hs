91d0b4c058163a3f46f030cbf22011d97eed4c4d
Markus Kupke <markus.kupke@sap.com>
2018-12-07 10:54:42 +0100

    Dirks Challenge

---
+++ wuerfeln.hs
@@ -0,0 +1,14 @@
+import Data.List
+
+newThrowForExistingCombination :: (Num a, Enum a) => [a] -> [[a]]
+newThrowForExistingCombination [] = map (\x -> [x]) [1..6]
+newThrowForExistingCombination c = map (\(ls,l) -> l:ls) (zip (repeat c) [1..6])
+
+allThrowCombinationsSumGreaterM n m = foldr (combinator m n) [[]] (reverse [1..n])
+
+combinator :: Int -> Int -> Int -> [[Int]] -> [[Int]] 
+combinator m n j cs = generateNew cs
+            where generateNew cs =  filter (\xs -> sum xs >= (m - 6*(n-j) ))
+                                           (foldr (++) [] (map newThrowForExistingCombination cs))
+
+main = writeFile "out" (show (allThrowCombinationsSumGreaterM 10 34))
