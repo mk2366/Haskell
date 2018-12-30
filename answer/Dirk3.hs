7f2d6e73eb87b516530825d79bcde869900c0924
Markus Kupke <kupkemarkus@gmail.com>
2018-12-31 00:29:36 +0100

    Gute Nacht

--- answer/Dirk3.hs
+++ answer/Dirk3.hs
@@ -1,14 +1,14 @@
 import Data.List (delete, minimumBy)
 import Data.List.Index (indexed)
 
-compare' :: Ord a => (Int, a) -> (Int, a) -> Ordering
-compare' (a, b) (c,d) = compare b d 
 
 pullNearestPoint :: (Ord a, Fractional a) => a -> [a] -> Maybe (a,[a])
 pullNearestPoint _ [] = Nothing
 pullNearestPoint p xs = let
                            xs'     = map (abs . (-)p) xs
                            ixs     = indexed xs'
+                           compare' :: Ord a => (Int, a) -> (Int, a) -> Ordering
+                           compare' (a, b) (c,d) = compare b d 
                            (i, _)  = minimumBy compare' ixs
                            a       = xs !! i
                            xs''    = delete (xs !! i) xs
