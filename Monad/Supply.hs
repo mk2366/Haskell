d4cc89064c2387f33213a6e2915fc28c388a1615
Markus Kupke <kupkemarkus@gmail.com>
2019-01-23 21:22:51 +0100

    Immer mit der Ruhe

---
+++ Monad/Supply.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+
+module Supply
+    (
+      Supply
+    , next
+    , runSupply
+    ) where
+
+import Control.Monad.State
+
+newtype Supply s a = S (State [s] a) deriving (Monad,Applicative,Functor)
+
+runSupply :: Supply s a -> [s] -> (a, [s])
+
+next :: Supply s (Maybe s)
+
+next = S $ do st <- get
+              case st of
+                [] -> return Nothing
+                (x:xs) -> do put xs
+                             return (Just x)
+
+runSupply (S m) xs = runState m xs
